90b8426becb3f82a80f84a0d0229497de20e4d30
Evgeniy Golda <goldaev@gmail.com>
2019-03-23 11:11:25 +0300

    карты работают

--- app/src/main/java/com/golda/cleancodeproject/presentation/screen/vehicle/VehicleMapsActivity.kt
+++ app/src/main/java/com/golda/cleancodeproject/presentation/screen/vehicle/VehicleMapsActivity.kt
@@ -37,8 +37,8 @@ class VehicleMapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        val sydney = LatLng(53.8522285,27.5047414)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Minsk Forever"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
